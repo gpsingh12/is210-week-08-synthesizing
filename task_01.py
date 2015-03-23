6831576c60497bb8e414ca6dfb8a0c5157496b71
gpsingh12 <gurpreet_1441@yahoo.com>
2015-03-23 03:48:48 +0000

    edit3

--- task_01.py
+++ task_01.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python 
-# -*- coding: utf-8 -*- 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """Module creates versus matchup for players.
 """
+
+
 def get_matches(players):
     """ Function takes input as a list of players names.
         Arg:
@@ -15,9 +17,8 @@ def get_matches(players):
     player = []
     for myind1, myitem1 in enumerate(players):
         for myind2, myitem2 in enumerate(players):
-         if myind1 < myind2:
-             list1 = (myitem1, myitem2)
-             player.append(list1)
+            if myind1 < myind2:
+                list1 = (myitem1, myitem2)
+                player.append(list1)
 
     return player
- 
